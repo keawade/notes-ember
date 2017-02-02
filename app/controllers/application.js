f0ec330df24565ab5a6e50bfc5b575386d7903fb
Keith Wade <keawade@gmail.com>
2017-02-01 21:08:18 -0600

    Implement semantic and mock basic view

---
+++ app/controllers/application.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  notes: [
+    {
+      title: 'Note 1',
+      content: 'This is only a test 1',
+    },
+    {
+      title: 'Note 2',
+      content: 'This is only a test 2',
+    },
+    {
+      title: 'Note 3',
+      content: 'This is only a test 3',
+    },
+    {
+      title: 'Note 4',
+      content: 'This is only a test 4',
+    },
+    {
+      title: 'Note 5',
+      content: 'This is only a test 5',
+    },
+  ]
+});
